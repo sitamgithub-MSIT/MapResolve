69e32d584d263e9ceddc6ca3c7a87ef4b3068f70
Sitam <sitammeur@gmail.com>
2023-10-21 21:19:18 +0530

    added urls file for the app users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+# Importing the path function from django.urls
+from django.urls import path
+
+# Importing the views.py file from the users app
+from . import views
+
+# Setting the app_name variable to "users"
+app_name = "users"
+
+# Setting the urlpatterns variable to a list of paths
+urlpatterns = []
