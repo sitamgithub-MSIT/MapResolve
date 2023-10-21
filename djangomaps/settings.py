c1f5aec07b9e03931818347b55b5922df0296232
Sitam <sitammeur@gmail.com>
2023-10-21 19:44:17 +0530

    changed some things in settings file

--- djangomaps/settings.py
+++ djangomaps/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 
