c7d513fb60c41c0bf56bb8264bcca51b64abe81d
Sitam <sitammeur@gmail.com>
2023-10-23 15:31:53 +0530

    views imports added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# Django imports
+from django.shortcuts import redirect, render 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# Local imports
+from .forms import MyUserCreationForm, AuthForm, UserProfileForm
 
 # Create your views here.
