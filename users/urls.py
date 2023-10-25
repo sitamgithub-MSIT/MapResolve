d94964df655bd1a300920d96764c5945a798fb8d
Sitam <sitammeur@gmail.com>
2023-10-25 18:42:32 +0530

    url patterns change in users app

--- users/urls.py
+++ users/urls.py
@@ -10,13 +10,13 @@ app_name = "users"
 # Setting the urlpatterns variable to a list of paths
 urlpatterns = [
     # Path for the Account view
-    path("account/", views.AccountView.as_view(), name="account"),
+    path("", views.AccountView.as_view(), name="account"),
     # Path for the Profile view
-    path("profile/", views.profile_view, name="profile"),
+    path("profile", views.profile_view, name="profile"),
     # Path for the Register view
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("register", views.RegisterView.as_view(), name="register"),
     # Path for the Login view
-    path("login/", views.LoginView.as_view(), name="login"),
+    path("login", views.LoginView.as_view(), name="login"),
     # Path for the Logout view
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
 ]
