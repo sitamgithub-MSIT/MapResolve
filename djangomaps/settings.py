e4509e77e468f8f20499f9be2d35fd57ef190616
Sitam <sitammeur@gmail.com>
2023-10-25 18:26:16 +0530

    settings file some changes in env key

--- djangomaps/settings.py
+++ djangomaps/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv
 import os
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
