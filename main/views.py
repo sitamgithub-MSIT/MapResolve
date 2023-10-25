72774ea1e87d1ab4d8241cc69492bded55cf0cf1
Sitam <sitammeur@gmail.com>
2023-10-25 11:57:01 +0530

    small fixes in views file

--- main/views.py
+++ main/views.py
@@ -8,9 +8,9 @@ from djangomaps.mixins import Directions
 
 # Create your views here.
 
+
 # Function based view for the routing
 def routing(request):
-
     # Getting the context
     context = {
         "google_api_key": settings.GOOGLE_API_KEY,
@@ -21,9 +21,8 @@ def routing(request):
     return render(request, "main/path.html", context)
 
 
-# Function based view for displaying a map 
+# Function based view for displaying a map
 def map(request):
-
     # Getting the params
     lat_a = request.GET.get("lat_a")
     long_a = request.GET.get("long_a")
@@ -34,17 +33,21 @@ def map(request):
     lat_d = request.GET.get("lat_d")
     long_d = request.GET.get("long_d")
 
-    # Getting the directions
-    directions = Directions(
-        lat_a=lat_a,
-        long_a=long_a,
-        lat_b=lat_b,
-        long_b=long_b,
-        lat_c=lat_c,
-        long_c=long_c,
-        lat_d=lat_d,
-        long_d=long_d,
-    )
+    if lat_a and long_a and lat_b and long_b and lat_c and long_c and lat_d and long_d:
+        # Getting the directions
+        directions = Directions(
+            lat_a=lat_a,
+            long_a=long_a,
+            lat_b=lat_b,
+            long_b=long_b,
+            lat_c=lat_c,
+            long_c=long_c,
+            lat_d=lat_d,
+            long_d=long_d,
+        )
+    else:
+        # Redirecting to the routing page
+        return redirect(reverse("main:routing"))
 
     # Getting the context
     context = {
@@ -60,10 +63,8 @@ def map(request):
         "long_d": long_d,
         "origin": f"{lat_a},{long_a}",
         "destination": f"{lat_b},{long_b}",
-        "directions": directions
+        "directions": directions,
     }
-
 
     # Returning the response
     return render(request, "main/map.html", context)
-
