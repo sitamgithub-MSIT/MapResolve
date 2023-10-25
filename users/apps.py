4a7ca6c648a09ac2102db744ce80b76b714aa42f
Sitam <sitammeur@gmail.com>
2023-10-26 01:57:10 +0530

    added the signals to it

--- users/apps.py
+++ users/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    # Importing the signals module from the users app and connecting it to the ready method of the UsersConfig class
+    def ready(self):
+        import users.signals
