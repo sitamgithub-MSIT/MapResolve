b1ceb35b778e75b734f1d10185b929e40e235dd9
Sitam <sitammeur@gmail.com>
2023-10-25 11:09:33 +0530

    base country added in settings

--- djangomaps/settings.py
+++ djangomaps/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'users',
-    'base',
+    'main.apps.MainConfig',
+    'users.apps.UsersConfig',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 
@@ -134,3 +134,4 @@ RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+BASE_COUNTRY = 'US'
