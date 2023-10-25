813ab254d7e78d1357a68fded45ea2947467104d
Sitam <sitammeur@gmail.com>
2023-10-25 12:07:24 +0530

    urls of main app added

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,6 @@ app_name = "main"
 
 # Setting the urlpatterns variable to a list of paths
 urlpatterns = [
-    path("route", views.homepage, name=""),
-    path("map", views.about, name=""),
+    path("route", views.homepage, name="routing"),
+    path("map", views.about, name="mapsview"),
 ]
