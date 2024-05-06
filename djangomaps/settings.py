ad596eb8c9699a5e7cd26d188c58e7a444d6ddf1
Sitam <sitammeur@gmail.com>
2024-05-07 00:49:33 +0530

    admin site customisation done

--- djangomaps/settings.py
+++ djangomaps/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -135,6 +136,59 @@ RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+JAZZMIN_SETTINGS = {
+    "site_header": "MapResolve",
+    "site_brand": "MapResolve Admin",
+    # "site_logo": "images/logo.png",
+    "copyright": "MapResolve ltd",
+    "welcome_sign": "Welcome to MapResolve Admin, Login now !",
+    "topmenu_links": [],
+    "order_with_respect_to": [
+        "base",
+        "main",
+        "users",
+    ],
+    "icons": {
+        "admin.LogEntry": "fas fa-file",
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "userauths.User": "fas fa-user",
+        "userauths.Profile": "fas fa-address-card",
+    },
+    "show_ui_builder": True,
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": "navbar-indigo",
+    "accent": "accent-olive",
+    "navbar": "navbar-indigo navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-indigo",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
+    },
+}
 
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "users:account"
--- users/admin.py
+++ users/admin.py
@@ -5,5 +5,42 @@ This module registers the UserProfile model with the Django admin site.
 from django.contrib import admin
 from .models import UserProfile
 
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the UserProfile model.
+
+    This class defines the display, filtering, and search options for the UserProfile model in the Django admin interface.
+
+    Attributes:
+        list_display (tuple): A tuple of field names to be displayed in the admin list view.
+        list_filter (tuple): A tuple of field names to be used for filtering in the admin list view.
+        search_fields (list): A list of field names to be used for searching in the admin list view.
+
+    """
+
+    list_display = (
+        "user",
+        "address",
+        "town",
+        "county",
+        "postcode",
+        "country",
+        "captcha_score",
+        "has_profile",
+        "is_active",
+    )
+    list_filter = ("has_profile", "is_active")
+    search_fields = [
+        "user__username",
+        "user__email",
+        "address",
+        "town",
+        "county",
+        "postcode",
+        "country",
+    ]
+
+
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
